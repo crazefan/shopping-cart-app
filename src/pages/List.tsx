6fb7db7b0ae453b656a1c970955267b4f2aff0ea
dolgopolovt <dolgopolov.devtest@gmail.com>
2021-12-18 20:26:51 +0100

    add items fetching and grid render

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,7 +1,55 @@
+import { useEffect, useState } from "react";
+
 import Box from "@mui/material/Box";
+import ProductCard from "../components/product/ProductCard";
+import Grid from "@mui/material/Grid";
+
+export type ProductItemType = {
+  id: number;
+  name: string;
+  price: number;
+  available: boolean;
+  brand: string;
+  weight?: number;
+  options?: {
+    color?: string[];
+    quantity?: number;
+    storage?: string[];
+    power: number[];
+  }[];
+};
 
 const List = () => {
-  return <Box>This is list component</Box>;
+  const [items, setItems] = useState<ProductItemType[]>([]);
+  const [cartItems, setCartItems] = useState<ProductItemType[]>([]);
+
+  const getItems = () => {
+    fetch("products.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((products) => setItems(products.items))
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    getItems();
+  }, []);
+
+  return (
+    <Box>
+      <Grid container rowSpacing={1} columnSpacing={1}>
+        {items.map((item) => (
+          <Grid key={item.id} item xs={4}>
+            <ProductCard key={item.id} item={item} />
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
 };
 
 export default List;
